f3883cac2438a2a35cb48d27656643d389c2f426
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-11-11 01:20:44 -0800

    cleanup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,28 +6,14 @@ function App() {
   const [entryLevel, setEntryLevel] = useState<Boolean>(false);
 
   useEffect(() => {
-    /**
-     * We can't use "chrome.runtime.sendMessage" for sending messages from React.
-     * For sending messages from React we need to specify which tab to send it to.
-     */
-    chrome.tabs && chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, tabs => {
-      /**
-       * Sends a single message to the content script(s) in the specified tab,
-       * with an optional callback to run when a response is sent back.
-       *
-       * The runtime.onMessage event is fired in each content script running
-       * in the specified tab for the current extension.
-       */
-
+    chrome.tabs && chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
       chrome.tabs.sendMessage(
         tabs[0]['id'] || 0,
         { type: 'GET_DOM' } as DOMMessage,
         (response: DOMMessageResponse) => {
           setEntryLevel(response.entryLevel);
-        });
+        }
+      );
     });
   }, []);
 
