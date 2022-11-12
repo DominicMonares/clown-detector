efa1f4845a1435d90bdc08259445cb17e26fd190
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-11-12 03:42:57 -0800

    implement blacklist state

--- src/components/Blacklist.tsx
+++ src/components/Blacklist.tsx
@@ -1,27 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, MouseEvent } from 'react';
 
-import { BlacklistSetting, BlacklistProps } from '../types';
+import { BlacklistProps } from '../types';
 
 const Blacklist = ({ updateBlacklist, blacklist }: BlacklistProps) => {
   const [newKeyword, setNewKeyword] = useState<string>('');
 
-  const triggerUpdateBl = (toAdd: string, toRemove: string) => {
-    updateBlacklist(newKeyword, toRemove);
-    if (!toRemove) setNewKeyword('');
+  const addToBlacklist = () => {
+    updateBlacklist(newKeyword, '');
+  }
+
+  const removeFromBlacklist = (e: MouseEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    if (target['parentNode'] !== null) {
+      const keyword = target['parentNode']['children'][0]['innerHTML'];
+      updateBlacklist('', keyword);
+    }
   }
 
   return (
     <div>
-      <input
-        type='text'
-        placeholder='Add to blacklist'
-        onChange={(e) => setNewKeyword(e.target.value)}
-        value={newKeyword}
-      />
-      <button onClick={() => triggerUpdateBl(newKeyword, '')}>
-        Add to blacklist
-      </button>
-      {Array.from(blacklist).map(b => b)}
+      <div>
+        <input
+          type='text'
+          placeholder='Add to blacklist'
+          onChange={(e) => setNewKeyword(e.target.value)}
+          value={newKeyword}
+        />
+        <button onClick={addToBlacklist}>
+          Add to blacklist
+        </button>
+      </div>
+      <div>
+        {Array.from(blacklist).map(b => {
+          return (
+            <div key={b}>
+              <span>{b}</span>
+              <span onClick={removeFromBlacklist}>X</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
