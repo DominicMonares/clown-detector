ddf63f751230bca8e8322ed8301ca8ef93973c9a
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-11-16 16:09:03 -0800

    add url updater

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -2,8 +2,7 @@ chrome.tabs.onUpdated.addListener(
   (tabId, changeInfo, tab) => {
     if (changeInfo.url) {
       chrome.tabs.sendMessage( tabId, {
-        message: 'newURL',
-        url: changeInfo.url
+        urlUpdated: true
       });
     }
   }
