c9eba982841dab6c0e8cd707a3f30e52dba0af4d
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-11-18 01:49:43 -0800

    move applySettings

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -1,26 +1,20 @@
-chrome.runtime.onInstalled.addListener(() => {
-  // Disabled by default
-  chrome.action.disable();
-  chrome.action.setIcon({ path: '/inactive_icon-32.png' });
-});
+import { ReactMessageRes, Settings } from '../types';
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   if (changeInfo.url) {
-    const url = changeInfo.url;
-    const searchUrl = 'linkedin.com/jobs/search/?currentJobId';
-    const viewUrl = 'linkedin.com/jobs/view/';
-    const searchUrlValid = url.includes(searchUrl);
-    const viewUrlValid = url.includes(viewUrl);
-
-    if (searchUrlValid || viewUrlValid) {
-      chrome.action.enable();
-      chrome.action.setIcon({ path: '/active_icon-32.png' });
-      chrome.tabs.sendMessage(tabId, { urlUpdated: true });
-    } else {
-      chrome.action.disable();
-      chrome.action.setIcon({ path: '/inactive_icon-32.png' });
-    }
+    chrome.tabs.sendMessage(tabId, { urlUpdated: true });
   }
 });
 
-export {};
+export const applySettings = ({ entryLevel, clownlist }: Settings) => {
+  chrome.tabs && chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    chrome.tabs.sendMessage(
+      tabs[0]['id'] || 0,
+      { settings: { entryLevel, clownlist } },
+      (res: ReactMessageRes) => {}
+    );
+  });
+
+  chrome.tabs.reload();
+  window.close();
+}
