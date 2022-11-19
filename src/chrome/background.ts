70473d08258a699240b69629f2e239c38efcd411
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-11-19 01:09:26 -0800

    cleanup

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -1,17 +1,17 @@
-import { ReactMessageRes, Settings } from '../types';
+import { Settings } from '../types';
 
+
+// Tell content script to run scanJob after url updates
 chrome.tabs && chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-  if (changeInfo.url) {
-    chrome.tabs.sendMessage(tabId, { urlUpdated: true });
-  }
+  if (changeInfo.url) chrome.tabs.sendMessage(tabId, { urlUpdated: true });
 });
 
+// Tell content script to save new settings to storage
 export const applySettings = ({ entryLevel, clownlist }: Settings) => {
   chrome.tabs && chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     chrome.tabs.sendMessage(
       tabs[0]['id'] || 0,
-      { settings: { entryLevel, clownlist } },
-      (res: ReactMessageRes) => {}
+      { settings: { entryLevel, clownlist } }
     );
   });
 
--- src/chrome/content.ts
+++ src/chrome/content.ts
@@ -1,5 +1,5 @@
-import { escape } from 'html-escaper';
 import $ from 'jquery';
+import { escape } from 'html-escaper';
 import {
   ReactMessageRes,
   ReactMessageListener,
@@ -8,17 +8,19 @@ import {
   ScanJob,
   Years
 } from '../types';
-
-import { createELKeywords, createFlag, replaceApostrophes, waitForTopCard } from './helpers';
+import {
+  createELKeywords,
+  createFlag,
+  replaceApostrophes,
+  waitForTopCard
+} from './helpers';
 
 
 let settings: Settings;
 
 // Function called when a new message is received
 const reactMessageListener: ReactMessageListener = (msg, sender, sendResponse) => {
-  console.log('[content.js]. Message received', msg);
-
-  // Handle settings request from app
+  // Send stored settings back to app when requested
   if (!Object.keys(msg).length) {
     chrome.storage.sync.get(['entryLevel', 'clownlist'], res => {
       const storedSettings: Settings = {
@@ -34,9 +36,8 @@ const reactMessageListener: ReactMessageListener = (msg, sender, sendResponse) =
       sendResponse(response);
       return true;
     });
-
-  // Handle updated url
   } else if (msg.urlUpdated) {
+    // Run job scan when updated url is sent from background
     waitForTopCard(scanJob, settings);
     const response: ReactMessageRes = {
       status: 'Successfully fetched settings!',
@@ -45,11 +46,10 @@ const reactMessageListener: ReactMessageListener = (msg, sender, sendResponse) =
 
     sendResponse(response);
     return true;
-
-  // Handle settings update
   } else if (msg.settings) {
+    // Store settings sent from app
     settings = msg.settings;
-    chrome.storage.sync.set(msg.settings, () => {});
+    chrome.storage.sync.set(msg.settings, () => { });
   }
 
   return true;
@@ -57,21 +57,21 @@ const reactMessageListener: ReactMessageListener = (msg, sender, sendResponse) =
 
 chrome.runtime.onMessage.addListener(reactMessageListener);
 
-// Scan job and flag keywords
+// Scan job description html and flag keywords
 const scanJob: ScanJob = (topCard, { entryLevel, clownlist }) => {
   const clownlistKeys = Object.keys(clownlist);
 
-  // Check if job is listed explicitly as entry-level
+  // Check if job is explicitly listed as entry level
   const isEntryLevel = topCard.includes('Entry level');
   if (!isEntryLevel || (!entryLevel && !clownlistKeys)) return;
 
   // Combine clownlist with entry level variations
-  const years = entryLevel + 2 as Years;
+  const years = entryLevel + 2 as Years; // Add 2 to account for marks
   const clownlistKeywords = replaceApostrophes(clownlistKeys);
   const entryLevelKeywords = entryLevel ? createELKeywords(years, []) : [];
   const allKeywords = entryLevelKeywords.concat(clownlistKeywords);
 
-  // Get job html as a string and look for keywords
+  // Get job html as a string and search for keywords
   const job = $('#job-details')[0].outerHTML.toLowerCase();
   const flaggedKeywords = allKeywords.filter(k => {
     return job?.includes(k.toLowerCase()) ? true : false;
@@ -82,16 +82,14 @@ const scanJob: ScanJob = (topCard, { entryLevel, clownlist }) => {
   createFlag(escapedKeywords);
 }
 
-// Get settings and run scan on page load
+// Get settings and run job scan once page is loaded
 window.onload = () => {
   chrome.storage.sync.get(['entryLevel', 'clownlist'], res => {
     if (!Object.keys(res).length) {
+      // Create default settings if user settings don't exist
       const defaultEntryLevel: EntryLevelSetting = 5;
       settings = { entryLevel: defaultEntryLevel, clownlist: {} };
-      chrome.storage.sync.set(settings, () => {
-        console.log('Default values set!');
-        waitForTopCard(scanJob, settings);
-      });
+      chrome.storage.sync.set(settings, () => waitForTopCard(scanJob, settings));
     } else {
       settings = { entryLevel: res.entryLevel, clownlist: res.clownlist };
       waitForTopCard(scanJob, settings);
