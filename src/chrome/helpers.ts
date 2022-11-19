7364868ee4e8db19a63ecedd03934a20e433fd9c
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-11-18 22:53:36 -0800

    implement replaceApostrophes

--- src/chrome/helpers.ts
+++ src/chrome/helpers.ts
@@ -19,19 +19,30 @@ export const waitForTopCard: WaitForTopCard = (callback, settings) => {
   }, 500);
 }
 
+export const replaceApostrophes = (keywords: string[]) => {
+  return keywords.map(k => {
+    return k.includes("'") ? k.split('').map(v => v === "'" ? '’' : v).join('') : k;
+  });
+}
+
 export const createELKeywords: CreateELKeywords = (years, keywords) => {
   suffixes.forEach(s => keywords.push(years + s));
   const nextYear = years + 1 as Years;
-  if (years === 15) {
-    return keywords;
-  } else {
-    return createELKeywords(nextYear, keywords);
-  }
+  return years === 15 ? keywords : createELKeywords(nextYear, keywords);
 }
 
 export const createFlag = (keywords: string[]) => {
-  const topCard = $(`.${topCardClassName}`)[0];
-  const newHTML = `&#129313;`;
+  const joinedKeywords = (keywords.map((k, i) => {
+    return i === keywords.length - 1 ? k : k + ' · ';
+  })).join('');
+
+  const topCard = $(`.${topCardClassName}`).first();
+  const newHTML = $(`
+    <div style="display:flex; align-items:center; margin-left:12px;">
+      <font size="5" style="margin-right:10px">&#129313;</font>
+      <span>${joinedKeywords}</span>
+    </div>
+  `);
 
   topCard.append(newHTML);
 }
