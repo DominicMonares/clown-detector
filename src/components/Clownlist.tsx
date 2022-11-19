aad9dcc2df9133217ddcb56d49eb6191a2fb308c
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-11-18 23:35:39 -0800

    refactor handlers

--- src/components/Clownlist.tsx
+++ src/components/Clownlist.tsx
@@ -1,18 +1,31 @@
-import { useState, MouseEvent, KeyboardEvent } from 'react';
+import {
+  useState,
+  ChangeEvent,
+  KeyboardEvent,
+  MouseEvent
+} from 'react';
 
 import { ClownlistProps } from '../types';
 
 
 const Clownlist = ({ updateClownlist, clownlist }: ClownlistProps) => {
   const [newKeyword, setNewKeyword] = useState<string>('');
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(true);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value;
+    input ? setButtonDisabled(false) : setButtonDisabled(true);
+    setNewKeyword(input);
+  }
+
+  const handleEnter = (e: KeyboardEvent) => {
+    if (e.key === 'Enter' && !buttonDisabled) addToClownlist();
+  }
 
   const addToClownlist = () => {
     updateClownlist(newKeyword, '');
     setNewKeyword('');
-  }
-
-  const handleEnter = (e: KeyboardEvent) => {
-    if (e.key === 'Enter') addToClownlist();
+    setButtonDisabled(true);
   }
 
   const removeFromClownlist = (e: MouseEvent<HTMLInputElement>) => {
@@ -31,11 +44,15 @@ const Clownlist = ({ updateClownlist, clownlist }: ClownlistProps) => {
           className='clownlist-textfield'
           type='text'
           placeholder='NFT, unpaid, etc.'
-          onChange={(e) => setNewKeyword(e.target.value)}
+          onChange={handleChange}
           onKeyUp={handleEnter}
           value={newKeyword}
         />
-        <button className='cd-button' onClick={addToClownlist}>
+        <button
+          className='cd-button'
+          onClick={addToClownlist}
+          disabled={buttonDisabled}
+        >
           <b>Add</b>
         </button>
       </div>
