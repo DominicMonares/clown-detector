8764a157940b3fcae708a049ef963d11ea5dfb9a
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-11-19 23:03:38 -0800

    remove unused dependencies

--- src/tests/render.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../components/App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Entry Level Threshold/i);
-  expect(linkElement).toBeInTheDocument();
-});
