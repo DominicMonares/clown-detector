9d4caf7caaa1c04878205ee5af33b6984d6fb156
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-11-19 17:50:47 -0800

    implement body

--- src/components/Offsite.tsx
+++ src/components/Offsite.tsx
@@ -1,7 +1,11 @@
+import sad_clown from '../assets/sad_clown.png';
+
 const Offsite = () => {
   return (
-    <div>
-      Offsite
+    <div className='offsite'>
+      <img src={sad_clown} alt='sad clown' />
+      <p>Uh-oh! Looks like you aren't on a LinkedIn job post.</p>
+      <p>Please visit the LinkedIn job board to use Clown Detector.</p>
     </div>
   );
 }
