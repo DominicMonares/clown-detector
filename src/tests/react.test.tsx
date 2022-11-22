5864d8e040bd751d207c4c2760208715d24438ac
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-11-22 01:47:51 -0800

    implement Clownlist input test

---
+++ src/tests/react.test.tsx
@@ -0,0 +1,78 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../components/App';
+import EntryLevel from '../components/EntryLevel';
+import Clownlist from '../components/Clownlist';
+import Offsite from '../components/Offsite';
+Object.assign(global, require('jest-chrome'));
+
+
+describe('App Component', () => {
+  it('should render initial layout', () => {
+    render(<App />);
+    const entryLevel = screen.getByText(/Entry Level Threshold/i);
+    expect(entryLevel).toBeInTheDocument();
+    const clownlist = screen.getByText(/Clownlisted Keywords/i);
+    expect(clownlist).toBeInTheDocument();
+    const submit = screen.getByText(/Apply settings and reload/i);
+    expect(submit).toBeInTheDocument();
+    const report = screen.getByText(/Report a bug/i);
+    expect(report).toBeInTheDocument();
+  });
+});
+
+describe('Entry Level Component', () => {
+  it('should render initial layout', () => {
+    const spy = jest.fn();
+    render(<EntryLevel updateEntryLevel={spy} defaultSlider={5} />);
+    const title = screen.getByText(/Entry Level Threshold/i);
+    expect(title).toBeInTheDocument();
+    const disabled = screen.getByText(/Disabled/i);
+    expect(disabled).toBeInTheDocument();
+    const sevenYears = screen.getByText(/7 years/i);
+    expect(sevenYears).toBeInTheDocument();
+  });
+});
+
+describe('Clownlist Component', () => {
+  it('should render initial layout', () => {
+    const mock = jest.fn();
+    const clownlist = { silly: true, job: true };
+    render(<Clownlist updateClownlist={mock} clownlist={clownlist} />);
+
+    const title = screen.getByText(/Clownlisted Keywords/i);
+    expect(title).toBeInTheDocument();
+    const input = screen.getByPlaceholderText(/NFT, unpaid, etc./i);
+    expect(input).toBeInTheDocument();
+    const addButton = screen.getByText(/Add/i);
+    expect(addButton).toBeInTheDocument();
+    const clearButton = screen.getByText(/Clear/i);
+    expect(clearButton).toBeInTheDocument();
+    const keyword1 = screen.getByText(/silly/i);
+    expect(keyword1).toBeInTheDocument();
+    const keyword2 = screen.getByText(/job/i);
+    expect(keyword2).toBeInTheDocument();
+  });
+
+  it('should trigger update when user input submitted', async () => {
+    const user = userEvent.setup();
+    const mock = jest.fn();
+    const clownlist = {};
+    render(<Clownlist updateClownlist={mock} clownlist={clownlist} />);
+    const input = screen.getByPlaceholderText(/NFT, unpaid, etc./i);
+    await user.type(input, 'haberdasher');
+    await user.keyboard('{Enter}');
+    expect(mock).toHaveBeenCalled();
+  });
+});
+
+describe('Offsite Component', () => {
+  render(<Offsite />);
+  const clown = screen.getAllByAltText(/sad clown/i)[0];
+  expect(clown).toBeVisible();
+  const p1 = screen.getByText(/Uh-oh! Looks like you aren't on a LinkedIn job post./i);
+  expect(p1).toBeInTheDocument();
+  const p2 = screen.getByText(/Please visit the LinkedIn job board to use Clown Detector./i);
+  expect(p2).toBeInTheDocument();
+});
