ab599dac435c147c5f1a54391a57426c4e8839e7
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-11-26 19:35:27 -0800

    remove children from observer

--- src/chrome/content.ts
+++ src/chrome/content.ts
@@ -88,13 +88,10 @@ const scanJob: ScanJob = (topCard, { entryLevel, clownlist }) => {
 
 // Run job scan whenever the job list or details rerender
 const startObserver = () => {
-  const config = { attributes: true, childList: true, subtree: true };
+  const config = { attributes: true, subtree: true };
   const targetNode = $('.scaffold-layout__inner')[0];
   if (targetNode) {
-    const observer = new MutationObserver(() => {
-      console.log('ASSHOLE')
-      waitForTopCard(scanJob, settings, 0);
-    });
+    const observer = new MutationObserver(() => waitForTopCard(scanJob, settings, 0));
     observer.observe(targetNode, config);
   }
 }
