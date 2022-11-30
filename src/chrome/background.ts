a99fd922b1cc9c2c4dc3145ccdedc57319e903af
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-11-29 16:50:15 -0800

    implement back button handler

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -6,22 +6,27 @@ const tabTracker: { [key: string]: string } = {};
 // Re-inject content script after every page update
 const injectContent = () => {
   chrome.tabs.query({}, async tabs => {
+    // Get first content script from manifest
     const manifest = chrome.runtime.getManifest();
     const contentScript = manifest.content_scripts ? manifest.content_scripts[0] : null;
     const contentJs = contentScript ? contentScript.js : null;
     const contentJsFile = contentJs ? contentJs[0] : null;
     if (contentJsFile) {
       for (let tab of tabs) {
-        if (tab.status === 'complete' && tab.id) {
-          if (!tabTracker[tab.id]) tabTracker[tab.id] = tab.url || '';
+        if (tab.status === 'complete' && tab.id && tab.url) {
+          // Store tab id and corresponding url, compare current to previous each injection
+          if (!tabTracker[tab.id]) tabTracker[tab.id] = tab.url;
           const currentURL = tab.url;
           const previousURL = tabTracker[tab.id];
           const jobsURL = 'linkedin.com/jobs';
+          tabTracker[tab.id] = currentURL;
           if (currentURL?.includes(jobsURL)) {
-            console.log('URLS ', currentURL, previousURL)
+            // Manually reload if using back button on non-job LinkedIn page
+            // Not doing so breaks the job post event listeners
             if (!previousURL.includes(jobsURL) && previousURL.includes('linkedin.com')) {
               chrome.tabs.reload();
             } else {
+              tabTracker[tab.id] = currentURL;
               chrome.scripting.executeScript({
                 target: { tabId: tab.id as number, allFrames: true },
                 files: [contentJsFile]
