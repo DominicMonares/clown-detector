1e8f7c98d5856749a175478aecc53fa2fe525961
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2023-02-08 20:51:48 -0800

    add Indeed

--- src/components/Offsite.tsx
+++ src/components/Offsite.tsx
@@ -4,7 +4,7 @@ const Offsite = () => {
   return (
     <div className='offsite'>
       <img src={sad_clown} alt='sad clown' />
-      <p>Uh-oh! Looks like you aren't on a LinkedIn job post.</p>
+      <p>Uh-oh! Looks like you aren't on a LinkedIn or Indeed job post.</p>
       <p>Please visit the LinkedIn or Indeed job board to use Clown Detector.</p>
     </div>
   );
